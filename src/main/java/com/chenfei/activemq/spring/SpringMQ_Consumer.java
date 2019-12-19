52153b637c76a60dbb4bea339f2594702869851c
chen <18955928961@163.com>
2019-12-19 17:01:23 +0800

    second-commit

--- src/main/java/com/chenfei/activemq/spring/SpringMQ_Consumer.java
+++ src/main/java/com/chenfei/activemq/spring/SpringMQ_Consumer.java
@@ -1,7 +1,22 @@
 package com.chenfei.activemq.spring;
 
-public class SpringMQ_Consumer {
-	public static void main(String[] args){
+import org.apache.xbean.spring.context.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
 
+import javax.jms.Message;
+
+@Service
+public class SpringMQ_Consumer {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+	public static void main(String[] args){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		SpringMQ_Consumer consumer = (SpringMQ_Consumer) context.getBean("springMQ_Consumer");
+		String  retVale = (String) consumer.jmsTemplate.receiveAndConvert();
+		System.out.println("*******消费者收到的消息："+ retVale);
 	}
 }
